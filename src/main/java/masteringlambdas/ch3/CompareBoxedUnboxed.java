a010eca187985e1fb1518788a370e149f4229c09
Maurice Naftalin <maurice@morninglight.co.uk>
2014-07-10 15:21:48 +0100

    Put boxed and unboxed on the same footing, using an array as source

--- src/main/java/masteringlambdas/ch3/CompareBoxedUnboxed.java
+++ src/main/java/masteringlambdas/ch3/CompareBoxedUnboxed.java
@@ -1,15 +1,11 @@
 package mastering.performance.ch3;
 
 import org.openjdk.jmh.annotations.*;
-import org.openjdk.jmh.logic.BlackHole;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 import java.util.OptionalInt;
 import java.util.stream.IntStream;
-
-import static java.util.stream.Collectors.toList;
 
 @State(Scope.Benchmark)
 @Fork(1)
@@ -20,27 +16,27 @@ public class CompareBoxedUnboxed {
 
     private final int P = 4;
 
-    private List<Integer> integerList;
+    private Integer[] integerArray;
     private int[] intArray;
 
     @Setup(Level.Trial)
     public void setUp() {
-        integerList = IntStream.range(0, N).boxed().collect(toList());
+        integerArray = IntStream.range(0, N).boxed().toArray(Integer[]::new);
         intArray = IntStream.range(0,N).toArray();
         System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", Integer.toString(P));
     }
 
-    @GenerateMicroBenchmark
+    @Benchmark
     public Optional<Integer> boxed() {
-        return integerList.stream()
-                .map(i -> i + 1)
+        return Arrays.stream(integerArray)
+                .map(i -> i * 5)
                 .max(Integer::compareTo);
     }
 
-    @GenerateMicroBenchmark
+    @Benchmark
     public OptionalInt unboxed() {
         return Arrays.stream(intArray)
-                .map(i -> i + 1)
+                .map(i -> i * 5)
                 .max();
     }
 }
